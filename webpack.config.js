3528b34638295bbea5271d448abc5cc67b118bdd
Anlei-liu <1020043202@qq.com>
2017-10-06 18:07:35 +0800

    modify html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         app: ['webpack-hot-middleware/client','./src/app.js'],
-        vendor: ['react']
+        vendor: [
+            'react',
+            'react-dom'
+        ]
     },
     output: {
-        filename: 'entry.js',
+        filename: '[name].[hash].js',
         path: path.resolve(__dirname, 'public')
     },
     module: {
@@ -46,9 +49,28 @@ module.exports = {
         new ExtractTextPlugin('styles.css'),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.optimize.CommonsChunkPlugin(),
-        new webpack.ProvidePlugin({
-            React: 'react'
+        new webpack.HashedModuleIdsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        // new webpack.ClearWebpackPlugin(['public']),
+        new HtmlWebpackPlugin({
+            title: 'app',
+            templateContent: function (templateParams, compilation) {
+                return `<!doctype html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport"
+                                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                            <title>Document</title>
+                        </head>
+                        <body>
+                            <div id="main"></div>
+                        </body>
+                        </html>`;
+            },
         })
     ]
 };
